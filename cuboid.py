13c8c46e26a7261995bedf5c96315406f023afbe
Harisankar <116871620+Harisankar10@users.noreply.github.com>
2024-12-02 22:16:01 -0800

    Create cuboid.py

---
+++ cuboid.py
@@ -0,0 +1,8 @@
+
+def cuboid_surface(lengthh, breadthh, heightt):
+   
+    return 2 * (lengthh * breadthh + breadthh * heightt + heightt * lengthh)
+
+def cuboid_volume(lengthh, breadthh, heightt):
+   
+    return lengthh * breadthh * heightt
